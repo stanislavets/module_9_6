9dc736b7b9e412d140260718c6d38f3edcd1dbe0
stanislavets <stanislavets2009@mail.ru>
2024-09-09 20:12:58 +0300

    Add files via upload

---
+++ module_9_6.py
@@ -0,0 +1,10 @@
+
+def all_variants(text):
+    for x in range (len(text)):
+        for y in range (len(text)-x):
+            yield text[y:y+x+1]
+
+print()
+a = all_variants('abc')
+for i in a:
+    print(i)
